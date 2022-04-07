ba318c2162f0551fb98b1742110a7d14e16a4e8a
BB <me@yomama.com>
2022-04-07 14:33:07 +0300

    lake + river

--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 Given a center latitude, longitude and a zoom level simply plot all
 country boundaries that fit on the map
 
+Datafiles, Preprocessing
+
 Country Border Datafile comes from [here](https://thematicmapping.org/downloads/world_borders.php)
+
+Lake data is from [NOAA](https://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/)
+
+
 
--- util.py
+++ util.py
@@ -4,33 +4,38 @@ import numpy as np
 import shapefile
 import pandas as pd
 
-def preprocess_GSHHS_f_L2():
+def preprocess_GSHHS():
 
-    sf = shapefile.Reader("/home/burak/Downloads/GSHHS_f_L2.shp")
-    r = sf.records()
-    countries = sf.shapes()
-
-    print (len(countries))
+    files = ["/home/burak/Downloads/gshhg-shp-2.3.7/GSHHS_shp/f/GSHHS_f_L2.shp",
+             "/home/burak/Downloads/gshhg-shp-2.3.7/WDBII_shp/f/WDBII_river_f_L02.shp"]
+    
     res = []
-    for idx in range(len(countries)):
-        country = countries[idx]
-        name = r[idx]
-        print (name)
-        print (len(country.parts))
-        bounds = list(country.parts) + [len(country.points)]
-        for (previous, current) in zip(bounds, bounds[1:]):
-            geo = [[x[1],x[0]] for x in country.points[previous:current]]
-            if len(geo) < 1: continue
-            nvecs = np.array([LatLon(a[0],a[1]).toNvector() for a in geo])
-            mid = nvecs.mean().toLatLon()
-            res.append([mid.lat,mid.lon,geo])
+    for file in files:
+        print (file)
+        sf = shapefile.Reader(file)
+        r = sf.records()
+        countries = sf.shapes()
+
+        print (len(countries))
+        for idx in range(len(countries)):
+            country = countries[idx]
+            name = r[idx]
+            print (name)
+            print (len(country.parts))
+            bounds = list(country.parts) + [len(country.points)]
+            for (previous, current) in zip(bounds, bounds[1:]):
+                geo = [[x[1],x[0]] for x in country.points[previous:current]]
+                if len(geo) < 1: continue
+                nvecs = np.array([LatLon(a[0],a[1]).toNvector() for a in geo])
+                mid = nvecs.mean().toLatLon()
+                res.append([mid.lat,mid.lon,geo])
 
     df = pd.DataFrame(res)
     df.columns = ['lat','lon','polygon']
-    df.to_csv('/tmp/GSHHS_f_L2.csv',index=None)
+    df.to_csv('/tmp/lake_river.csv',index=None)
 
 
 if __name__ == "__main__": 
     
-    preprocess_GSHHS_f_L2()
+    preprocess_GSHHS()
     
