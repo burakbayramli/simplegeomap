dd00579dcaed62dfb3e6c957995ba0535626d7d2
BB <me@yomama.com>
2022-04-08 15:20:03 +0300

    elev 2

---
+++ tst-elev2.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from skimage.measure import block_reduce
+import random, itertools
+from geotiff import GeoTiff 
+import matplotlib.pyplot as plt
+from scipy.interpolate import Rbf
+
+tiff_file = "/home/burak/Downloads/dem_geotiff/DEM_geotiff/alwdgg.tif"
+
+area_box = ((25,34), (46,43))
+
+g = GeoTiff(tiff_file, crs_code=4326, as_crs=4326,  band=0)
+arr = g.read_box(area_box)
+arr = np.flip(arr,axis=0)
+print (arr.shape)
+
+X = np.linspace(area_box[0][0],area_box[1][0],arr.shape[1])
+Y = np.linspace(area_box[0][1],area_box[1][1],arr.shape[0])
+
+X,Y = np.meshgrid(X,Y)
+
+
+#rbfi = Rbf(X2,Y2,arr2)
+#arrhat = rbfi(X,Y)
+#arrhat[arrhat<0] = 0.0
+#CS=plt.contour(X,Y,arrhat)
+#plt.clabel(CS, fontsize=10, inline=1)
+#plt.savefig('out4.png')
